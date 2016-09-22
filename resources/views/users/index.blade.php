7b530dce15d919283810008801cb40b28cc22506
mwatson <mattwat@gmail.com>
2016-09-22 17:13:24 -0400

    add admin badge icon

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -12,6 +12,7 @@
             <div class="panel-body">
                 @foreach ($users as $user)
                     <a href="{{ route('users.show', $user) }}"><img src="{{ Gravatar::get($user->email, ['size' => 30]) }}" class="avatar"> {{ $user->name }}</a>
+                    {{ $user->admin ? '&#128219;' : '' }}
                     <br><br>
                 @endforeach
             </div>
